3e319aa1411e0229d76cca98c9bd747653518abf
SergioGonzalez24 <sergiomanuel2020@gmail.com>
2021-05-15 22:28:20 -0500

    Creacion de codigo para el header

--- include/Enviador.hpp
+++ include/Enviador.hpp
@@ -15,7 +15,15 @@ class Enviador  {
 		//Constructores
 		Enviador();
 		Enviador(string nombre, int id_sucursal, string fecha_envio);
-		
 
+		//Geteres
+		string get_nombre() {return nombre;}
+		int get_id_sucursal() {return id_sucursal;}
+		string get_fecha_envio() {return fecha_envio;}
+
+		//Seters
+		void set_nombre(string nombre);
+		void set_id_sucursal(string id_sucursal);
+		void set_fecha_env(string fecha_envio);
 };
 #endif
